577c3a785fc1e05fb7b049ff47de2110fb58b9d2
BerryHeyy <berkaykaplanbk@hotmail.com>
2023-07-21 15:31:04 +0200

    Fixed saving if buffer wasn't suitable

--- lua/session-manager/init.lua
+++ lua/session-manager/init.lua
@@ -76,17 +76,21 @@ local function get_session_path()
 	return ensure_path_is_safe(config.session_path)
 end
 
+local function is_buffer_suitable()
+	local opened_with_args = next(vim.fn.argv()) ~= nil
+	local reading_from_stdin = vim.g.in_pager_mode == 1
+
+	return not opened_with_args and not reading_from_stdin
+end
+
 local function autocmd_save_session()
-	if vim.fn.filereadable(get_session_path()) == 1 then
+	if vim.fn.filereadable(get_session_path()) == 1 and is_buffer_suitable() then
 		session_manager.save_session()
 	end
 end
 
 local function autocmd_load_session()
-	local opened_with_args = next(vim.fn.argv()) ~= nil
-	local reading_from_stdin = vim.g.in_pager_mode == 1
-
-	if not opened_with_args and not reading_from_stdin then
+	if is_buffer_suitable() then
 		session_manager.load_session()
 	end
 end
